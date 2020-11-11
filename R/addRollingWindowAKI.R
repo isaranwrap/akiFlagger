829a14cbdb332e353c4fba1e0d0cc3239e6a8394
Ishan Saran <ishansaran65@gmail.com>
2020-11-10 20:07:03 -0800

    Added main functions, unit tests, the toy dataset, and associated package files.

--- R/addRollingWindowAKI.R
+++ R/addRollingWindowAKI.R
@@ -1,11 +1,48 @@
+#' Add in rolling-window AKI
+#'
+#' Add in the AKI column in a patient dataframe according to the rolling-window KDIGO criterion
+#'
+#' @param dataframe patient dataset
+#' @param window1 rolling window length of the shorter time window; defaults to 48 hours
+#' @param window2 rolling window length of the longer time window; defaults to 162 hours
+#' @param add_min_creat boolean to add the intermediate columns generated during calculation
+#'
+#'
+#' @return patient dataset with the rolling-window AKI column added in
+#'
+#' #Imports
+#' @import zoo
+#' @importFrom dplyr select
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr %>%
+#'
+#' @importFrom data.table fread
+#' @importFrom data.table last
+#' @importFrom data.table first
+#' @importFrom data.table copy
+#' @importFrom data.table :=
+#' @importFrom data.table .SD
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' addRollingWindowAKI(df)
+#' }
+
 addRollingWindowAKI <- function(dataframe, window1 = as.difftime(2, units='days'), window2 = as.difftime(7, units='days'),
                                 add_min_creat = FALSE) {
   # TEMP RETURN TO CHECK FUNCTION WORKS PROPERLY
-  return(paste0(g))
+  #return(paste0(getwd()))
+  patient_id <- encounter_id <- time <- creatinine <- NULL
+  condition1 <- condition2 <- stage1 <- stage2 <- stage3 <- NULL
+  min_creat48 <- min_creat7d <- NULL
+
   df <- dataframe %>% group_by(patient_id) %>%
     mutate(
-      min_creat48 = sapply(time, function(x) min(creatinine[between(time, x - window1, x)])), # Find minimum creatinine in the past 2 days
-      min_creat7d = sapply(time, function(x) min(creatinine[between(time, x - window2, x)])), # Find minimum creatinine in the past 7 days
+      min_creat48 = sapply(time, function(x) min(creatinine[zoo::between(time, x - window1, x)])), # Find minimum creatinine in the past 2 days
+      min_creat7d = sapply(time, function(x) min(creatinine[zoo::between(time, x - window2, x)])), # Find minimum creatinine in the past 7 days
 
       condition1 = creatinine >= min_creat48 + 0.3, # Check if the creat jumps by 0.3; aka KDIGO criterion 1
       condition2 = round(creatinine, digits=4) >= round(1.5*min_creat7d, digits=4), # Check if the creat jumps by 50%; aka KDIGO criterion 2
